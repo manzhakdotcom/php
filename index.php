337a7187e9076cd1277c69ea4de41170ae21a941
Sergey Manzhak <manzhak.com@gmail.com>
2017-06-07 19:26:11 +0300

    Add css and js

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ $view = new \App\View();
 $view->title = 'My site!';
 $view->users = \App\Models\User::findAll();
 $view->authors = \App\Models\Author::findAll();
-var_dump($view->authors);
+
 $view->news = \App\Models\News::findAll();
-var_dump($view->news);
-die;
+
+
 $view->display(__DIR__ . '/app/templates/index.php');
